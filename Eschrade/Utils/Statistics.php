f36de687f26e4c7c3f9b98880cd43a64ab1e7892
Kevin Schroeder <kschroeder@mirageworks.com>
2013-04-03 12:40:39 -0500

    Update Statistics.php

--- Eschrade/Utils/Statistics.php
+++ Eschrade/Utils/Statistics.php
@@ -2,36 +2,58 @@
 
 class Eschrade_Util_Statics
 {
-  
-	const EAV_BACKEND			= 'backend';
-	const EAV_TYPE				= 'type';
-	const EAV_TABLE				= 'table';
-	const EAV_FRONTEND			= 'frontend';
-	const EAV_INPUT				= 'input';
-	const EAV_LABEL				= 'label';
-	const EAV_FRONTEND_CLASS	= 'frontend_class';
-	const EAV_SOURCE			= 'source';
-	const EAV_REQUIRED			= 'required';
-	const EAV_USER_DEFINED		= 'user_defined';
-	const EAV_DEFAULT			= 'default';
-	const EAV_UNIQUE			= 'unique';
-	const EAV_NOTE				= 'note';
-	const EAV_GLOBAL			= 'global';
-	const EAV_VISIBLE			= 'visible';
-	const EAV_SEARCHABLE		= 'searchable';
-	const EAV_FILTERABLE		= 'filterable';
-	const EAV_COMPARABLE		= 'comparable';
-	const EAV_VISIBLE_ON_FRONT	= 'visible_on_front';
-	const EAV_WYSIWIG			= 'wysiwyg_enabled';
-	const EAV_HTML_ON_FRONT		= 'is_html_allowed_on_front';
-	const EAV_IN_ADVANCED_SEARCH	= 'visible_in_advanced_search';
-	const EAV_FILTERABLE_IN_SEARCH	= 'filterable_in_search';
+	
+	const EAV_BACKEND_ALIAS					= 'backend';
+	const EAV_TYPE_ALIAS					= 'type';
+	const EAV_TABLE_ALIAS					= 'table';
+	const EAV_FRONTEND_ALIAS				= 'frontend';
+	const EAV_INPUT_ALIAS					= 'input';
+	const EAV_LABEL_ALIAS					= 'label';
+	const EAV_SOURCE_ALIAS					= 'source';
+	const EAV_REQUIRED_ALIAS				= 'required';
+	const EAV_USER_DEFINED_ALIAS			= 'user_defined';
+	const EAV_DEFAULT_ALIAS					= 'default';
+	const EAV_UNIQUE_ALIAS					= 'unique';
+	const EAV_GLOBAL_ALIAS					= 'global';
+	const EAV_VISIBLE_ALIAS					= 'visible';
+	const EAV_SEARCHABLE_ALIAS				= 'searchable';
+	const EAV_FILTERABLE_ALIAS				= 'filterable';
+	const EAV_COMPARABLE_ALIAS				= 'comparable';
+	const EAV_VISIBLE_ON_FRONT_ALIAS		= 'visible_on_front';
+	const EAV_WYSIWIG_ALIAS					= 'wysiwyg_enabled';
+	const EAV_IN_ADVANCED_SEARCH_ALIAS		= 'visible_in_advanced_search';
+	const EAV_FILTERABLE_IN_SEARCH_ALIAS	= 'filterable_in_search';
+	const EAV_USED_FOR_PROMO_RULES_ALIAS	= 'used_for_promo_rules';
+		
+	const EAV_BACKEND				= 'backend_type';
+	const EAV_TYPE					= 'backend_type';
+	const EAV_TABLE					= 'backend_table';
+	const EAV_FRONTEND				= 'frontend_model';
+	const EAV_INPUT					= 'frontend_input';
+	const EAV_LABEL					= 'frontend_label';
+	const EAV_FRONTEND_CLASS		= 'frontend_class';
+	const EAV_SOURCE				= 'source_model';
+	const EAV_REQUIRED				= 'is_required';
+	const EAV_USER_DEFINED			= 'is_user_defined';
+	const EAV_DEFAULT				= 'default_value';
+	const EAV_UNIQUE				= 'is_unique';
+	const EAV_NOTE					= 'note';
+	const EAV_GLOBAL				= 'is_global';
+	const EAV_VISIBLE				= 'is_visible';
+	const EAV_SEARCHABLE			= 'is_searchable';
+	const EAV_FILTERABLE			= 'is_filterable';
+	const EAV_COMPARABLE			= 'is_comparable';
+	const EAV_VISIBLE_ON_FRONT		= 'is_visible_on_front';
+	const EAV_WYSIWIG				= 'is_wysiwyg_enabled';
+	const EAV_HTML_ON_FRONT			= 'is_html_allowed_on_front';
+	const EAV_IN_ADVANCED_SEARCH	= 'is_visible_in_advanced_search';
+	const EAV_FILTERABLE_IN_SEARCH	= 'is_filterable_in_search';
 	const EAV_IN_PRODUCT_LISTING	= 'used_in_product_listing';
-	const EAV_USED_FOR_SORT		= 'used_for_sort_by';
-	const EAV_APPLY_TO			= 'apply_to';
-	const EAV_POSITION			= 'position';
-	const EAV_CONFIGURABLE		= 'is_configurable';
-	const EAV_USED_FOR_PROMO_RULES	= 'used_for_promo_rules';
+	const EAV_USED_FOR_SORT			= 'used_for_sort_by';
+	const EAV_APPLY_TO				= 'apply_to';
+	const EAV_POSITION				= 'position';
+	const EAV_CONFIGURABLE			= 'is_configurable';
+	const EAV_USED_FOR_PROMO_RULES	= 'is_used_for_promo_rules';
 
 	const EAV_TYPE_DATETIME			= 'datetime';
 	const EAV_TYPE_INT				= 'int';
