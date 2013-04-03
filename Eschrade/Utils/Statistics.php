5129a84bcabf88de53849194d5ee1ed6a88e507b
Kevin Schroeder <kschroeder@mirageworks.com>
2013-04-03 08:47:55 -0500

    Create Statistics.php

---
+++ Eschrade/Utils/Statistics.php
@@ -0,0 +1,52 @@
+<?php
+
+class Eschrade_Util_Statics
+{
+  
+	const EAV_BACKEND			= 'backend';
+	const EAV_TYPE				= 'type';
+	const EAV_TABLE				= 'table';
+	const EAV_FRONTEND			= 'frontend';
+	const EAV_INPUT				= 'input';
+	const EAV_LABEL				= 'label';
+	const EAV_FRONTEND_CLASS	= 'frontend_class';
+	const EAV_SOURCE			= 'source';
+	const EAV_REQUIRED			= 'required';
+	const EAV_USER_DEFINED		= 'user_defined';
+	const EAV_DEFAULT			= 'default';
+	const EAV_UNIQUE			= 'unique';
+	const EAV_NOTE				= 'note';
+	const EAV_GLOBAL			= 'global';
+	const EAV_VISIBLE			= 'visible';
+	const EAV_SEARCHABLE		= 'searchable';
+	const EAV_FILTERABLE		= 'filterable';
+	const EAV_COMPARABLE		= 'comparable';
+	const EAV_VISIBLE_ON_FRONT	= 'visible_on_front';
+	const EAV_WYSIWIG			= 'wysiwyg_enabled';
+	const EAV_HTML_ON_FRONT		= 'is_html_allowed_on_front';
+	const EAV_IN_ADVANCED_SEARCH	= 'visible_in_advanced_search';
+	const EAV_FILTERABLE_IN_SEARCH	= 'filterable_in_search';
+	const EAV_IN_PRODUCT_LISTING	= 'used_in_product_listing';
+	const EAV_USED_FOR_SORT		= 'used_for_sort_by';
+	const EAV_APPLY_TO			= 'apply_to';
+	const EAV_POSITION			= 'position';
+	const EAV_CONFIGURABLE		= 'is_configurable';
+	const EAV_USED_FOR_PROMO_RULES	= 'used_for_promo_rules';
+
+	const EAV_TYPE_DATETIME			= 'datetime';
+	const EAV_TYPE_INT				= 'int';
+	const EAV_TYPE_TEXT				= 'text';
+	const EAV_TYPE_VARCHAR			= 'varchar';
+
+	const EAV_INPUT_BOOLEAN			= 'boolean';
+	const EAV_INPUT_DATE			= 'date';
+	const EAV_INPUT_FILE			= 'file';
+	const EAV_INPUT_HIDDEN			= 'hidden';
+	const EAV_INPUT_IMAGE			= 'image';
+	const EAV_INPUT_MULTILINE		= 'multiline';
+	const EAV_INPUT_MULTISELECT		= 'multiselect';
+	const EAV_INPUT_SELECT			= 'select';
+	const EAV_INPUT_TEXT			= 'text';
+	const EAV_INPUT_TEXTAREA		= 'textarea';
+	
+}
